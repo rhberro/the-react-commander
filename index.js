917053a1a80c738bc71f1ecb46bbd27b645d6ab1
Rafael Henrique Berro <rhberro@gmail.com>
2017-03-28 23:37:14 -0300

    Add an example of structure of commands.

--- index.js
+++ index.js
@@ -2,14 +2,35 @@
 
 const commander = require('commander')
 
+const commandNew = require('./lib/commands/new')
+const commandMakeComponent = require('./lib/commands/make/component')
+const commandMakeView = require('./lib/commands/make/view')
+const commandAddReducer = require('./lib/commands/add/reducer')
+const commandAddAction = require('./lib/commands/add/action')
+
 commander.version('0.0.0', '-v, --version')
-commander.usage('<command> [options]')
+commander.usage('the-react-commander <command> [options]')
 
-const forge = require('./lib/commands/forge')
+commander.command('new <name>')
+  .description('Create a new application.')
+  .usage('new <name> [options]')
+  .action(commandNew)
 
-commander.command('forge <name>')
-  .description('Forge a new application.')
-  .action(forge)
+commander.command('make:component <name>')
+  .description('Make a new component.')
+  .action(commandMakeComponent)
+
+commander.command('make:view <name>')
+  .description('Make a new view.')
+  .action(commandMakeView)
+
+commander.command('add:reducer <name>')
+  .description('Add a new reducer.')
+  .action(commandAddReducer)
+
+commander.command('add:action <name>')
+  .description('Add a new action.')
+  .action(commandAddAction)
 
 if (process.argv.length === 2) commander.help()
 
