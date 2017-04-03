8b0c9b2cf4306529eeb17f3876ff712f814777d2
Rafael Henrique Berro <rhberro@gmail.com>
2017-04-02 22:15:30 -0300

    Remove some unnecessary commands.

--- index.js
+++ index.js
@@ -5,32 +5,24 @@ const commander = require('commander')
 const commandNew = require('./lib/commands/new')
 const commandMakeComponent = require('./lib/commands/make/component')
 const commandMakeView = require('./lib/commands/make/view')
-const commandAddReducer = require('./lib/commands/add/reducer')
-const commandAddAction = require('./lib/commands/add/action')
 
 commander.version('0.0.0', '-v, --version')
 commander.usage('the-react-commander <command> [options]')
 
 commander.command('new <name>')
   .description('Create a new application.')
-  .usage('new <name> [options]')
+  .usage('<name> [options]')
   .action(commandNew)
 
 commander.command('make:component <name>')
   .description('Make a new component.')
+  .usage('<name> [options]')
   .action(commandMakeComponent)
 
 commander.command('make:view <name>')
   .description('Make a new view.')
+  .usage('<name> [options]')
   .action(commandMakeView)
-
-commander.command('add:reducer <name>')
-  .description('Add a new reducer.')
-  .action(commandAddReducer)
-
-commander.command('add:action <name>')
-  .description('Add a new action.')
-  .action(commandAddAction)
 
 if (process.argv.length === 2) commander.help()
 
