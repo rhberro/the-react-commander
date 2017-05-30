40ce1ecd60a16e7e34cccc0ff94c4d0a0904c5f7
Rafael Henrique Berro <rhberro@gmail.com>
2017-05-29 21:33:16 -0300

    Fix the replace module tests.

--- test/utilities/replace.test.js
+++ test/utilities/replace.test.js
@@ -6,28 +6,28 @@ describe( 'the replace module', () => {
     const currentMarkup = '<h1><%name%></h1>'
     const currentProperties = { name: 'Example' }
     const desiredMarkup = '<h1>Example</h1>'
-    expect( replace.apply(currentMarkup, currentProperties) ).toMatch( desiredMarkup )
+    expect( replace(currentMarkup, currentProperties) ).toMatch( desiredMarkup )
   } )
 
   it( 'should replace two properties', () => {
     const currentMarkup = '<h1><%name%></h1><h2><%uname%></h2>'
     const currentProperties = { name: 'Example', uname: 'Subtitle' }
     const desiredMarkup = '<h1>Example</h1><h2>Subtitle</h2>'
-    expect( replace.apply(currentMarkup, currentProperties) ).toMatch( desiredMarkup )
+    expect( replace(currentMarkup, currentProperties) ).toMatch( desiredMarkup )
   } )
 
   it( 'should replace two times the same property', () => {
     const currentMarkup = '<h1><%name%></h1><h2><%name%></h2>'
     const currentProperties = { name: 'Example' }
     const desiredMarkup = '<h1>Example</h1><h2>Example</h2>'
-    expect( replace.apply(currentMarkup, currentProperties) ).toMatch( desiredMarkup )
+    expect( replace(currentMarkup, currentProperties) ).toMatch( desiredMarkup )
   } )
 
   it( 'should replace two times different properties', () => {
     const currentMarkup = '<h1><%uname%></h1><h2><%name%></h2><div><%uname%></div><span><%name%></span>'
     const currentProperties = { name: 'Example', uname: 'Subtitle' }
     const desiredMarkup = '<h1>Subtitle</h1><h2>Example</h2><div>Subtitle</div><span>Example</span>'
-    expect( replace.apply(currentMarkup, currentProperties) ).toMatch( desiredMarkup )
+    expect( replace(currentMarkup, currentProperties) ).toMatch( desiredMarkup )
   } )
 } )
 /* eslint-enable */
